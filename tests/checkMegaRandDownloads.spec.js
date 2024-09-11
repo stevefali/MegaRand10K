49ce240344aea520ac12bf2f12dae6821162060b
Steven <stevefaliszewski@gmail.com>
2024-09-11 15:03:41 -0700

    Run playwright test that checks Mega Randomizer downloads every minute and sends email if over 10,000

---
+++ tests/checkMegaRandDownloads.spec.js
@@ -0,0 +1,46 @@
+// @ts-check
+
+import { test, expect } from "@playwright/test";
+
+const notificationApi = require("notificationapi-node-server-sdk").default;
+
+require("dotenv").config();
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(
+    "https://www.curseforge.com/minecraft/mc-mods/mega-randomizer"
+  );
+});
+
+test("findDownloads", async ({ page }) => {
+  const downloads = await page.locator(".detail-downloads");
+
+  const downloadsQuantity = await downloads.innerText();
+
+  console.log("> 9k: ", Number(getDownloadsAsNumber(downloadsQuantity)) > 9000);
+
+  if (Number(getDownloadsAsNumber(downloadsQuantity)) >= 10000) {
+    notificationApi.send({
+      notificationId: "10k_downloads",
+      user: {
+        id: "stevefaliszewski@gmail.com",
+        email: "stevefaliszewski@gmail.com",
+      },
+    });
+    console.log("YAAAAAAAYYYYY!!!!!!");
+  }
+
+  await expect(downloads).toBeVisible();
+});
+
+function getDownloadsAsNumber(downloadsString) {
+  let asNum = "";
+  for (let i = 0; i < downloadsString.length; i++) {
+    if (!isNaN(downloadsString[i])) {
+      asNum += downloadsString[i];
+    }
+  }
+  return asNum;
+}
