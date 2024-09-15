a0a1c149d44a9152d4407e6539f45e09077bfe62
Steven <stevefaliszewski@gmail.com>
2024-09-15 16:27:18 -0700

    Refactor to use playwright library instead of Test

--- jobs/checkDownloads.js
+++ jobs/checkDownloads.js
@@ -1,5 +1,53 @@
-const shell = require("shelljs");
+const { chromium } = require("playwright");
+
+const notificationApi = require("notificationapi-node-server-sdk").default;
+
+require("dotenv").config();
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
 
 console.log("Checking..");
 
-shell.exec("npx playwright test");
+async function checkDownloadsWithPlaywright() {
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto(
+    "https://www.curseforge.com/minecraft/mc-mods/mega-randomizer"
+  );
+
+  const downloads = page.locator(".detail-downloads");
+
+  const downloadsQuantity = await downloads.innerText();
+
+  if (Number(getDownloadsAsNumber(downloadsQuantity)) >= 10000) {
+    // @ts-ignore
+    notificationApi.init(clientId, clientSecret);
+    notificationApi.send({
+      notificationId: "10k_downloads",
+      user: {
+        id: "stevefaliszewski@gmail.com",
+        email: "stevefaliszewski@gmail.com",
+      },
+    });
+    console.log("YAAAAAAAYYYYY!!!!!!");
+  }
+
+  await context.close();
+  await browser.close();
+}
+
+(async () => {
+  await checkDownloadsWithPlaywright();
+})();
+
+function getDownloadsAsNumber(downloadsString) {
+  let asNum = "";
+  for (let i = 0; i < downloadsString.length; i++) {
+    if (!isNaN(downloadsString[i])) {
+      asNum += downloadsString[i];
+    }
+  }
+  return asNum;
+}
